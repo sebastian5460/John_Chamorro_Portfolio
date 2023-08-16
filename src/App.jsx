a12e8e76d199f7b4ed35f91543c5bb7997584647
John020 <sebastianchamorro271@gmail.com>
2023-08-16 02:27:36 -0500

    gh-pages added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="John_Chamorro_Portfolio.github.io">
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
